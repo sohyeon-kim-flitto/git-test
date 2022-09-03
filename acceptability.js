f597c716cff28b99d27d355c44e3e7910c976763
sohyeonkim <sohyeon.kim@flitto.com>
2022-09-03 14:48:08 +0900

    feat: AT 작업 완료
    feat: task1 작업 완료
    
    feat: task2 작업 완료

--- acceptability.js
+++ acceptability.js
@@ -1,1 +1,12 @@
 //ATATATATATATATATAT 입니당
+
+function task1() {
+  console.log('task1 입니당')
+  console.log('task1 작업 완료!!')
+}
+
+function task2() {
+  console.log('task2 입니당')
+  console.log('task2 진행중!!')
+  console.log('task2 작업 완료!!!!')
+}
